9781155c5ef3d2a44c577281172f6bc6fa3e2036
ianm <idmasse@icloud.com>
2023-12-05 17:48:22 -0800

    adds google login/out middleware and updates header.ejs

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ app.use(session({
   saveUninitialized: true
 }))
 
+app.use(function(req, res, next) { 
+  res.locals.user = req.user
+  next()
+})
+
 app.use(passport.initialize())
 app.use(passport.session())
 
