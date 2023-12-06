128eec3b202964154835235a490408aac6b6649b
ianm <idmasse@icloud.com>
2023-12-05 17:45:08 -0800

    adds google oauth login/out routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate( 'google', { scope: ['profile', 'email'] } ))
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate( 'google', { successRedirect: '/meetings', failureRedirect: '/meetings'} ))
+// OAuth logout route
+router.get('/logout', (req, res) => req.logout( () => res.redirect('/meetings')) )
 
 module.exports = router;
