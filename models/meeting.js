cc3ad5b5e562f012d389d8853fff5aab89300596
ianm <idmasse@icloud.com>
2023-12-05 17:15:13 -0800

    adds models and controller architecture

---
+++ models/meeting.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+    content: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    userName: String,
+    userAvatar: String
+  }, {
+    timestamps: true
+  })
+
+
+const meetingSchema = new Schema({
+
+})
+
+module.exports = mongoose.model('User', userSchema)
