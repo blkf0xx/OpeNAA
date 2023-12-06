4a4e7fbda427b17f56f9c3a7958d6c146958da72
ianm <idmasse@icloud.com>
2023-12-05 18:11:54 -0800

    adds ejs pages

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ router.get('/', function(req, res, next) {
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate( 'google', { scope: ['profile', 'email'] } ))
 // Google OAuth callback route
-router.get('/oauth2callback', passport.authenticate( 'google', { successRedirect: '/meetings', failureRedirect: '/meetings'} ))
+router.get('/oauth2callback', passport.authenticate( 'google', { successRedirect: '/meetings', failureRedirect: '/'} ))
 // OAuth logout route
-router.get('/logout', (req, res) => req.logout( () => res.redirect('/meetings')) )
+router.get('/logout', (req, res) => req.logout( () => res.redirect('/')) )
 
 module.exports = router;
