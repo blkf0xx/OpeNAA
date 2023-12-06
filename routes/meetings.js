99db178e760f5396da047eaba8b49b6821f3ff2c
ianm <idmasse@icloud.com>
2023-12-06 13:22:07 -0800

    adds routes for index and new

--- routes/meetings.js
+++ routes/meetings.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const meetingsCtrl = require('../controllers/meetings')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get('/', meetingsCtrl.index)
+router.get('/new', meetingsCtrl.new)
 
 module.exports = router;
