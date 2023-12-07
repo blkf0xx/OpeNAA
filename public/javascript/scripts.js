745a98aeb96211a1f0be1eea43758c964926c2bb
ianm <idmasse@icloud.com>
2023-12-07 11:10:11 -0800

    updates scripts

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,13 +1,20 @@
+document.addEventListener('DOMContentLoaded', showFields)
+
+// function to show or hide form elements based on selected options on /meetings/new
+function handleFormatChange() {
+    const formatValue = document.getElementById('format').value
+    const dependentInputs = document.getElementById('dependent-inputs')
+    const location = document.getElementById('location')
+  
+    dependentInputs.style.display = formatValue === 'Hybrid' || formatValue === 'Online Only' ? 'block' : 'none'
+    location.style.display = formatValue === 'Hybrid' || formatValue === 'In Person' ? 'block' : 'none'
+  }
+
+  document.getElementById('format').addEventListener('change', handleFormatChange)
+
 // function to show or hide meeting info on /meetings/:id
 function showFields() {
-  const zoomInfoDiv = document.getElementById('zoom-info');
-  const mtgFormatDiv = document.getElementById('mtg-format').innerText.trim();
-  zoomInfoDiv.style.display = mtgFormatDiv === 'In Person' ? 'none' : 'block';
+  const zoomInfoDiv = document.getElementById('zoom-info')
+  const mtgFormatDiv = document.getElementById('mtg-format').innerText.trim()
+  zoomInfoDiv.style.display = mtgFormatDiv === 'In Person' ? 'none' : 'block'
 }
-
-showFields()
-
-
-  
-
-
