20c3442da08b902a0c6f7fba9bde4f901bfaceb1
ianm <idmasse@icloud.com>
2023-12-06 21:55:19 -0800

    adds script to hide/show form ui on the add meetings page

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,13 +1,8 @@
 // function to show or hide meeting info on /meetings/:id
 function showFields() {
-  const zoomInfoDiv = document.getElementById('zoom-info')
-  const mtgFormatDiv = document.getElementById('mtg-format').innerText.trim()
-  console.log(mtgFormatDiv)
-  if (mtgFormatDiv === 'In Person') {
-    zoomInfoDiv.style.display = 'none'
-  }  else {
-    zoomInfoDiv.style.display = 'block'
-  }
+  const zoomInfoDiv = document.getElementById('zoom-info');
+  const mtgFormatDiv = document.getElementById('mtg-format').innerText.trim();
+  zoomInfoDiv.style.display = mtgFormatDiv === 'In Person' ? 'none' : 'block';
 }
 
 showFields()
