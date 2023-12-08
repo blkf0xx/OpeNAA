8a88983dd34a48682ae0772853f0b246e1f0a799
ianm <idmasse@icloud.com>
2023-12-08 01:20:28 -0800

    updates model

--- models/meeting.js
+++ models/meeting.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const commentSchema = new Schema({
-    content: { type: String, required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    userAvatar: String
-  }, {
-    timestamps: true
-  })
+  content: { type: String, required: true },
+  name: String,
+  avatar: String
+}, { timestamps: true })
+
 
 const meetingSchema = new Schema({
-  userMtg: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  userMtg: { type: Schema.Types.ObjectId, ref: 'User' },
   mtgName: String,
   organization: { type: String, enum: ['AA', 'NA'] },
   weekday: { type: String, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] },
@@ -18,7 +17,7 @@ const meetingSchema = new Schema({
   length: Number,
   description: String,
   format: { type: String, enum: ['In Person', 'Online Only', 'Hybrid'] },
-  link: { type: String, trim: true},
+  link: String,
   mtgId: String,
   mtgPasscode: String,
   address: String,
@@ -30,13 +29,6 @@ const meetingSchema = new Schema({
 }, { timestamps: true
 })
 
- // Trim leading and trailing whitespaces
-meetingSchema.pre('save', (next) => {
-  if (this.link) {
-    this.link = this.link.trim()
-  }
-  next()
-})
 
 module.exports = mongoose.model('meeting', meetingSchema)
 
