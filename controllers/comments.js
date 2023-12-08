295d8956fd13c8f279dedfa668df631cf35005cb
ianm <idmasse@icloud.com>
2023-12-08 01:20:53 -0800

    updates commment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,13 +5,26 @@ module.exports = {
 }
 
 async function create(req, res) {
-    const meeting = await Meeting.findById(req.params.id)
-    meeting.comments.push(req.body)
     try {
-        await meeting.save();
+        const meeting = await Meeting.findById(req.params.id)
+        
+        // anonymize last name
+        const splitName = req.user.name.split(' ')
+        const firstName = splitName[0]
+        const lastInitial = splitName.length > 1 ? splitName.pop().charAt(0).toUpperCase() : ''
+        const displayName = lastInitial ? `${firstName} ${lastInitial}.` : firstName
+
+        meeting.comments.push({
+            content: req.body.content,
+            name: displayName,
+            avatar: req.user.avatar
+        })
+
+        await meeting.save()
         res.redirect(`/meetings/${meeting._id}`)
     } catch (err) {
-        console.log(err);
+        console.log(err)
         res.redirect(`/meetings/${meeting._id}`)
     }
 }
+
