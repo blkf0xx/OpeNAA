edb0f51e099916cf2166893d0bdb626137895efe
ianm <idmasse@icloud.com>
2023-12-08 14:08:56 -0800

    adds functions to edit, update and delete meetings

--- controllers/meetings.js
+++ controllers/meetings.js
@@ -5,7 +5,10 @@ module.exports = {
     index,
     show,
     new: newMeeting,
-    create
+    create,
+    edit,
+    update,
+    delete:deleteMtg
 }
 
 async function index(req, res) {
@@ -15,7 +18,7 @@ async function index(req, res) {
 
 async function show(req, res) {
     const meetings = await Meeting.findById(req.params.id)
-    res.render('meetings/show', { title: 'Meeting Info' , meetings} )
+    res.render('meetings/show', { title: 'Meeting Info', meetings, currentUser: req.user } ) 
 }
 
 async function newMeeting(req, res) {
@@ -32,5 +35,38 @@ async function create(req, res) {
     } catch(err) {
         console.log(err.message)
         res.redirect('/meetings/new')
+    }
+}
+
+async function edit(req, res) {
+    const meeting = await Meeting.findById(req.params.id)
+    res.render('meetings/edit', { title: 'Update Meeting', meeting} )
+}
+
+async function update(req, res) {
+    try {
+        const updatedMtg = await Meeting.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new:true }
+        )
+        return res.redirect(`/meetings/${updatedMtg._id}`)
+    } catch(err) {
+        console.log(err)
+        res.redirect('/meetings')
+    }
+}
+
+async function deleteMtg(req, res) {
+    try {
+        const deletedMeeting = await Meeting.findOneAndDelete( { _id: req.params.id } )
+        if (!deletedMeeting) {
+            console.log('Meeting not found')
+            return res.redirect('/meetings')
+        }
+        res.redirect('/meetings')
+    } catch(err) {
+        console.log(err)
+        res.redirect('/meetings')
     }
 }
