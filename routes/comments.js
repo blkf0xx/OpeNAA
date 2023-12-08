9c5f41482810ce44289a749af175661eb90f5a95
ianm <idmasse@icloud.com>
2023-12-08 00:31:13 -0800

    adds comments route

---
+++ routes/comments.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const commentsCtrl = require('../controllers/comments')
+
+router.post('/meetings/:id/comments', commentsCtrl.create)
+
+
+module.exports = router
