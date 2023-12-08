4ac7e0718b8197c1f50f5ad49d38704aed7324e7
ianm <idmasse@icloud.com>
2023-12-08 00:30:48 -0800

    updates js code

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,4 @@
+document.getElementById('format').addEventListener('change', handleFormatChange)
 document.addEventListener('DOMContentLoaded', showFields)
 
 // function to show or hide form elements based on selected options on /meetings/new
@@ -10,11 +11,10 @@ function handleFormatChange() {
     location.style.display = formatValue === 'Hybrid' || formatValue === 'In Person' ? 'block' : 'none'
   }
 
-  document.getElementById('format').addEventListener('change', handleFormatChange)
-
 // function to show or hide meeting info on /meetings/:id
 function showFields() {
   const zoomInfoDiv = document.getElementById('zoom-info')
   const mtgFormatDiv = document.getElementById('mtg-format').innerText.trim()
   zoomInfoDiv.style.display = mtgFormatDiv === 'In Person' ? 'none' : 'block'
 }
+
